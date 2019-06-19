5d77551202400d346e026411a92a7d5da7f0c129
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-06-19 14:26:30 -0400

    - Created first tests  - Added steps to 1) launch browser. 2) navigate to Google, 3) Enter search terms 4) Click Search button, getting errors, cleaned some formatting 5) fixed an error that came from trying to click a Search button. Error was coming up because I needed to accept a EULA for something in intellij.. maybe? clicked to accept and the error stopped. 6) added implicit wait period of 5 seconds to search step to avoid timing issue/updated search terms 7) edited line 63 to search for a partial link name, that let selenium find the desired link and click. Added a short sleep timer to let me see the successful loading of the desired page before closing the browser
    2019-06-05
    1) Added steps to log into State Portal UI,
    2) kinda stuck trying to add an Assert
    3) Added steps to navigate to automationPractice.com, click a dress, add a quantity
    4) Fixed comments
    
    2019-06-06
    1) trying to add assert statement to check the math on the sample website. struggling to figure that out, fixed variable type to allow decimals
    2) figured out how to make chrome dev tools give me an xpath, used it to close the modal on the sample test site.
    3) Added simple asserts to each test
    4) NEXT STEP: Need to figure out how to store login inforation separately so it't not hard-coded in the script. That seems like a maintenance nightmare waiting to happen
    
    2019-06-10
    1) Cleaned up test steps to remove redundancies
     2) Moved ChromeDriver and maximize steps to Before class
     3) Moved Close step to After class
    
     2019-06-12
     1) Updated an assert to use getText, see line 79
    
     2019-06-14
     1) got cssSelector to work, sometimes
     2) Added steps to try completing a 'sale', stuck on Personal Info' section, can't find fields to enter name info
     3) Found issue with cssSelector (typo);
     4) Need to add steps to select checkboxes
    
    2019-06-17
    1) added step to 'Select from a List'
    1a) Selected Washington from drop down list of states
    2) removed redundant steps. edited comments
    
    2019-06-18
    1) Edited steps to randomize email address (starting step 184, thanks, Stack Overflow!)
    
    2019-06-19
    1) Cleaned up tests by moving more test steps to before/after classes

---
+++ src/test/java/com/tim/TimsFirstAutomatedTests.java
@@ -0,0 +1,152 @@
+package com.tim;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by hollisti on 2019-05-24.
+ */
+
+public class TimsFirstAutomatedTests {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void beforeClassSetup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\lib\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void afterClassCleanup() {
+        System.out.println("Clean up steps held in 'afterClass'");
+        driver.close();
+    }
+
+    @Before
+    public void beforeTestSetup() {
+
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+
+    @Test
+    public void productQuantityFieldValidations() throws InterruptedException {
+        //Testing using a sample site
+        //1. Open browser-moved to Before class
+//      #---Moved to Before Class---#
+
+//        2. Go to google.com
+//      #---Moved to Before Class---#
+
+//        3. Enter search criteria
+        driver.findElement(By.id("search_query_top")).sendKeys("summer");
+
+//        4. click Search button
+        driver.findElement(By.name("submit_search")).click();
+
+//        5. Click a link from search results
+        driver.findElement(By.partialLinkText("Printed Dress")).click();
+        String costPerItem = driver.findElement(By.className("ajax_block_products_total")).getText().replace("$", "");
+
+//        6. Add a large number in Quantity field
+        driver.findElement(By.id("quantity_wanted")).clear();
+        driver.findElement(By.id("quantity_wanted")).sendKeys("999");
+        driver.findElement(By.name("Submit")).click();
+
+//       7. Confirm cost of order
+        String actualAmount = driver.findElement(By.id("layer_cart_product_price")).getText().replace("$", "");
+        assertEquals("Total amount does not match", (999 * Double.valueOf(costPerItem)), actualAmount); //confirm amount matches expected
+    }
+
+    @Test
+    public void automationPracticeCheckout() throws InterruptedException {
+        //Testing using a sample site
+        //1. Open browser-moved to Before class
+
+//        2. Go to google.com
+        driver.get("http://automationpractice.com/index.php");
+
+//        3. Enter search criteria
+        driver.findElement(By.id("search_query_top")).sendKeys("faded");
+
+//        4. click Search button
+        driver.findElement(By.name("submit_search")).click();
+
+//        5. Click a link from search results
+        driver.findElement(By.partialLinkText("Faded Short Sleeve T-shirts")).click();
+
+//        6. Add a large number in Quantity field
+        driver.findElement(By.id("quantity_wanted")).clear();
+        driver.findElement(By.id("quantity_wanted")).sendKeys("1");
+        driver.findElement(By.name("Submit")).click();
+
+//       7. Confirm cost of order
+        assertEquals(16.51, (16.51 * 1), 0); //confirm amount matches expected
+        driver.findElement(By.xpath("//*[@id=\'layer_cart_product_price\']")).click();
+
+//        8. Proceed through checkout steps
+        String testEmail = UUID.randomUUID().toString();
+        testEmail = testEmail.substring(0, Math.min(testEmail.length(), 8)); // ---truncating the email address but not sure how!--- //
+        driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span")).click();
+        driver.findElement(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span")).click();
+        driver.findElement(By.id("email_create")).sendKeys(testEmail + "@testing.net");
+        Thread.sleep(15000); // slowing test to show off randomized email address
+        driver.findElement(By.id("SubmitCreate")).click();
+
+
+//        9. Name & Password
+        driver.findElement(By.xpath("//*[@id=\'id_gender1\']")).click();
+        driver.findElement(By.name("customer_firstname")).clear();
+        driver.findElement(By.name("customer_firstname")).sendKeys("Malcolm");
+        driver.findElement(By.name("customer_lastname")).clear();
+        driver.findElement(By.name("customer_lastname")).sendKeys("Reynolds");
+        driver.findElement(By.name("passwd")).clear();
+        driver.findElement(By.name("passwd")).sendKeys("12345");
+
+//         10. Address info
+        driver.findElement(By.cssSelector("#address1")).clear();
+        driver.findElement(By.cssSelector("#address1")).sendKeys("9121 Serenity Dr");
+        driver.findElement(By.cssSelector("#address2")).clear();
+        driver.findElement(By.cssSelector("#address2")).sendKeys("Suite 14");
+        driver.findElement(By.cssSelector("#city")).clear();
+        driver.findElement(By.cssSelector("#city")).sendKeys("Browncoat");
+
+//      Select State from a dropdown
+        Select stDropdown = new Select(driver.findElement(By.xpath("//*[@id=\'id_state\']")));
+        stDropdown.selectByVisibleText("Washington");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+//      Complete form and submit
+        driver.findElement(By.cssSelector("#postcode")).clear();
+        driver.findElement(By.cssSelector("#postcode")).sendKeys("99258");
+        driver.findElement(By.cssSelector("#phone_mobile")).clear();
+        driver.findElement(By.cssSelector("#phone_mobile")).sendKeys("509.555.8574");
+        driver.findElement(By.cssSelector("#alias")).clear();
+        driver.findElement(By.cssSelector("#alias")).sendKeys("Business Address");
+        driver.findElement(By.cssSelector("#submitAccount")).click();
+
+
+    }
+
+
+}
